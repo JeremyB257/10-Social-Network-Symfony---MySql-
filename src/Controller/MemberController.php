2c1800b74bd27c3120b468ffc8c055bbedd33a4e
Jerem <bertelootjerem@gmail.com>
2023-03-24 07:39:50 +0100

    secur member route

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 class MemberController extends AbstractController
 {
     #[Route('/membres', name: 'users.index')]

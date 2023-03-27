7a2f472cd8680bfcfe7dba88b1fb14f4a6377b4c
Jerem <bertelootjerem@gmail.com>
2023-03-27 15:11:40 +0200

    add like/ unlike post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,15 +28,20 @@ class HomeController extends AbstractController
     }
 
     #[Route('/like/{id}', name: 'home.like')]
-    public function like(Post $post): Response
+    public function like(Post $post, EntityManagerInterface $manager): Response
     {
-
+        $post->addUsersLike($this->getUser());
+        $manager->persist($post);
+        $manager->flush();
         return $this->redirectToRoute('home.index');
     }
     #[Route('/unlike/{id}', name: 'home.unlike')]
-    public function unlike(Post $post): Response
+    public function unlike(Post $post, EntityManagerInterface $manager): Response
     {
 
+        $post->removeUsersLike($this->getUser());
+        $manager->persist($post);
+        $manager->flush();
         return $this->redirectToRoute('home.index');
     }
 }
